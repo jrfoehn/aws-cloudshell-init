f047e0620315952bc4b69da22cd7aa5fdbf99d24
Jean-Robin Foehn <jrfoehn@gmail.com>
2020-12-17 11:49:08 +0100

    added helm

--- init.sh
+++ init.sh
@@ -6,20 +6,18 @@
 yum install openssl -y
 
 # Install eksctl
-
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-
 sudo mv -v /tmp/eksctl /usr/local/bin
-
 eksctl completion bash >> ~/.bash_completion
 . /etc/profile.d/bash_completion.sh
 . ~/.bash_completion
 
 # Install kubectl
-
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 
+# Install Helm
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
