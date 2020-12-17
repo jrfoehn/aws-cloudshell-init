8eaa848b3050249ed79c7188e98032cd09d055f2
Jean-Robin Foehn <jrfoehn@gmail.com>
2020-12-17 10:56:36 +0100

    first commit

---
+++ init.sh
@@ -0,0 +1,25 @@
+################
+# EKS Controls #
+################
+
+# Install OpenSSL
+yum install openssl -y
+
+# Install eksctl
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+sudo mv -v /tmp/eksctl /usr/local/bin
+
+eksctl completion bash >> ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+# Install kubectl
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+
